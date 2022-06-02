2c4a7827f2477df8976aa4ee2a640059323bc6e9
j1Karu <106725908+j1Karu@users.noreply.github.com>
2022-06-02 12:25:43 +0530

    Update GroupTest1.java

--- seleniumbasic/src/groupingintest/GroupTest1.java
+++ seleniumbasic/src/groupingintest/GroupTest1.java
@@ -1,25 +1,25 @@
 package groupingintest;
 
-// import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
-// public class GroupTest1 {
+public class GroupTest1 {
 	
-// 	@Test(groups = "Build" )
-// 	public void login () {
+	@Test(groups = "Build" )
+	public void login () {
 		
-// 		System.out.println("this is a Login from build");
-// 	}
+		System.out.println("this is a Login from build");
+	}
 
-// 	@Test (groups = "Build")
-// 	public void buildtest () {
+	@Test (groups = "Build")
+	public void buildtest () {
 		
-// 		System.out.println("this is a build test");
-// 	}
+		System.out.println("this is a build test");
+	}
 	
-// 	@Test (groups = "regression")
-// 	public void dashboard () {
+	@Test (groups = "regression")
+	public void dashboard () {
 		
-// 		System.out.println("this is a regression group dashboard test");
-// 	}
+		System.out.println("this is a regression group dashboard test");
+	}
 	
-// }
+}
